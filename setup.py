102a82cb7045485844aebb75c4ee46e26a1586fc
kageback <kageback@chalmers.se>
2019-06-05 22:12:41 +0200

    Added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 from setuptools import setup
 
 setup(name='pyAudioAnalysis',
-      version='0.2.5',
+      version='0.2.6',
       description='Python Audio Analysis Library: Feature Extraction, Classification, Segmentation and Applications',
       url='https://github.com/tyiannak/pyAudioAnalysis',
       author='Theodoros Giannakopoulos',
       author_email='tyiannak@gmail.com',
       license='Apache License, Version 2.0',
       packages=['pyAudioAnalysis'],
+      install_requires=[
+            'eyed3',
+            'libmagic',
+            'python-magic', #brew install libmagic on osx
+            'numpy',
+            'pydub',
+            'scipy',
+      ],
       zip_safe=False)
